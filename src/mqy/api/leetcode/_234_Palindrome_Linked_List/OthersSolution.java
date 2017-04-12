41ff66472ac328d9d708651aa50159d863008eb3
maoqiyun <maoqiyun@meituan.com>
2017-04-12 22:59:19 +0800

    gitignore

---
+++ src/mqy/api/leetcode/_234_Palindrome_Linked_List/OthersSolution.java
@@ -0,0 +1,44 @@
+package mqy.api.leetcode._234_Palindrome_Linked_List;
+
+/**
+ * Created by maoqiyun on 2017/4/12.
+ */
+public class OthersSolution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) { // odd nodes: let right half smaller
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+}
---
+++ src/mqy/api/leetcode/_234_Palindrome_Linked_List/Solution.java
@@ -0,0 +1,7 @@
+package mqy.api.leetcode._234_Palindrome_Linked_List;
+
+/**
+ * Created by maoqiyun on 2017/4/12.
+ */
+public class Solution {
+}
